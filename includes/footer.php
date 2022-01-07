31a2e6bea46bb34a5b59b96eafcade644f466824
Mohd Ubaid <mohdubaidzxc9@gmail.com>
2022-01-08 00:00:24 +0530

    make few changes on css

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
+<script src="assets/js/script.js"></script>
 <script src="lib/jquery/jquery.js"></script>
 <script src="lib/popper/popper.js"></script>
 <script src="lib/bootstrap/js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -2,68 +2,71 @@
 
 ?>
 <?php include 'includes/header.php'; ?>
-<link rel="stylesheet" href="assets/css/add-product.css">
+<link rel="stylesheet" href="assets/css/navbar.css">
+<link rel="stylesheet" href="assets/css/index.css">
 <?php include 'includes/navbar.php'; ?>
     <!-- Body Section -->
     <div class="body-bg m-2">
         <div class="heading my-3 px-4 border border-grey">
             <h3 class="px-1 py-3">Deals of the Day</h3>
             <div class="row">
-                <div class="col-md-2 pb-2">
+                <div class="col-md-2 pb-2 col-sm-6">
                     <div class="card" style="width: 200px;">
                         <span id="prod-avail">100</span>
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <a href="view-product.php" target="blank"> <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
                         <h5 class="text-center" >&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                        <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
                         <span id="prod-avail">100</span>
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                         <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
                         <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                        <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
                         <span id="prod-avail">100</span>
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                         <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
                         <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                        <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
                         <span id="prod-avail">100</span>
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                         <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
                         <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                        <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>   
@@ -74,58 +77,63 @@
         <div class="heading my-3 px-4">
             <h3 class="px-1 py-2">Discounts for You</h3>
             <div class="row">
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
                         <span id="prod-avail">100</span>
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-2">
+                <div class="col-md-2 col-sm-6">
                     <div class="card" style="width: 200px;">
-                        <img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" >
+                        <span id="prod-avail">100</span>
+                        <a href="view-product.php" target="blank"><img class="card-img-top" src="https://rukminim1.flixcart.com/image/150/150/kgv5x8w0/jacket/n/p/f/m-dtaw20jk023a-ducati-original-imafxyazyemfzzv4.jpeg?q=70" ></a>
                         <div class="card-body">
-                        <h5 class="text-center">&#x20b9 1500</h5>
-                        <a href="#" class="btn btn-primary btn-block" onclick="sell()">Sell</a>
+                            <h5 class="text-center">&#x20b9 1500</h5>
+                            <a href="#" class="btn btn-primary btn-block"  >Sell</a>
                         </div>
                     </div>
                 </div>  
@@ -134,5 +142,4 @@
     </div>
 
 
-<script src="assets/js/script.js"></script>
 <?php include 'includes/footer.php'; ?>
